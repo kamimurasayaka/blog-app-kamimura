d457038f76a8457b2780e852bda45468e2d1f065
kamimurasayaka <latte.flower@gmail.com>
2023-03-16 11:48:12 +0900

    request spec

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,9 +12,6 @@ class ArticlesController < ApplicationController
   def new
     @article = current_user.articles.build
   end
-
-  def like
-  end
   
   def create
     @article = current_user.articles.build(article_params)
